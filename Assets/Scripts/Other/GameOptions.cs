3a9ac561f6ca0604df6d93ee5c3e50579e2ad0d4
NayLiiac <levoyer.kylian24@gmail.com>
2024-08-19 13:23:30 +0200

    Drummer Anim et Settings completed

--- Assets/Scripts/Other/GameOptions.cs
+++ Assets/Scripts/Other/GameOptions.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class GameOptions : MonoBehaviour
 {
     public Animator OptionsAnimator;
 
+    [Header("Volume Settings")]
+    [SerializeField]
+    private AudioMixer _audioMixer;
     [SerializeField]
     private Slider _audioSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+    }
 
     public void OpenOptionWindow()
     {
@@ -18,5 +34,17 @@ public class GameOptions : MonoBehaviour
     public void CloseOptionWindow()
     {
         OptionsAnimator.SetTrigger("Close");
+    }
+
+    public void SetMusicVolume()
+    {
+        _audioMixer.SetFloat("MusicVolume", Mathf.Log10(_audioSlider.value)*20);
+        PlayerPrefs.SetFloat("MusicVolume", _audioSlider.value);
+    }
+
+    private void LoadVolume()
+    {
+        _audioSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        SetMusicVolume();
     }
 }
